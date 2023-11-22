0db1ab31fd632712e4fbc8202d079d60e1fe9839
CeeObi <140174587+CeeObi@users.noreply.github.com>
2023-11-23 01:20:34 +1100

    update error.jsx

--- joblista/src/Pages/Error.jsx
+++ joblista/src/Pages/Error.jsx
@@ -4,13 +4,12 @@ import { Link } from 'react-router-dom'
 
 
 const Error = () => {
-  return (<div className='h-screen '>
-        <div className='flex items-center justify-center'>
-          <img src={notFoundPics} alt="404 Picture" className='sm:w-3/5 lg:w-1/3  mt-7' />      
-        </div>
-        <h3 className='text-center font-semibold mt-8'>Oh! Page Not Found</h3>
-        <p className='text-center mt-4'>We can't seem to find the page you are looking for</p>
-        <Link to="/" ><h6 className='text-md text-primary underline text-center mt-10 font-medium'>Back Home</h6></Link>
+  return (
+    <div className='h-screen flex flex-col'>
+      <img src={notFoundPics} alt="404 Picture" className='w-2/3 sm:w-3/5 lg:w-1/3  mt-7 self-center mx-2' />      
+      <h3 className='text-center font-semibold mt-10'>Oh! Page Not Found</h3>
+      <p className='text-center mt-3'>We can't seem to find the page you are looking for</p>
+      <Link to="/" ><h6 className='text-md text-primary underline text-center mt-12 font-medium'>Back Home</h6></Link>
     </div>
   )
 }
